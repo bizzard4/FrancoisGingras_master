f717d50c8c547d952b4fd2ddd8bf4686dd993cf8
François Gingras <francois.gingras89@gmail.com>
2016-09-14 16:07:48 -0400

    Updated test to insert values

--- clist/list_test.c
+++ clist/list_test.c
@@ -4,6 +4,7 @@
 int main() {
 	List l;
     Position p;
+	Position five_pos;
 	int empty;
 
     l = MakeEmpty(NULL);
@@ -11,5 +12,19 @@ int main() {
 	empty = IsEmpty(l);
 	
 	printf("IsEmpty:%d\n", empty);
+	printf("Inserting 10 elemens\n");
+	for (int i = 0; i < 10; i++) {
+		Insert(i,l,p);
+		p = Advance(p);
+	}
+	printf("Done inserting\n");
+	empty = IsEmpty(l);
+	printf("IsEmpty:%d\n", empty);
+	
+	five_pos = Find(5, l);
+	int element = Retrieve(five_pos);
+	printf("5 found at:%d\n", element);
+	
+	
 	return 0;
 }
