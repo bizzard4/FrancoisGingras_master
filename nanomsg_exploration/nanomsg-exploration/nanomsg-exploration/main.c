ee09981e9a7e3e7fa2ed2c3101490f04433bef75
François Gingras <francois.gingras89@gmail.com>
2016-09-19 14:21:29 -0400

    Integrated c list and cleaned code

--- nanomsg_exploration/nanomsg-exploration/nanomsg-exploration/main.c
+++ nanomsg_exploration/nanomsg-exploration/nanomsg-exploration/main.c
@@ -1,21 +1,40 @@
 #include <stdio.h>
 #include "nn.h"
 #include "pair.h"
+#include "list.h"
 
 #define SOCKET_ADDRESS "inproc://fr_master1"
 
 int main() {
+	// Socket variable
+	int sa;
+	int sb;
+	// Connection variable
+	int ra;
+	int rb;
+	// Communication variables
+	char* data;
+	int len;
+	char* buf;
+	// Linked list variables
+	List list;
+	Position pos;
+
 	printf("Hello nanomsg\n");
 
+	// Prepare linked list
+	list = MakeEmpty(NULL);
+	pos = Header(list);
+
 	// Prepare nano socket
-	int sa = nn_socket(AF_SP, NN_PAIR);
+	sa = nn_socket(AF_SP, NN_PAIR);
 	if (sa < 0) {
 		printf("Error creating sa socket");
 		return -1;
 	}
 	printf("Socket a %d\n", sa);
 
-	int sb = nn_socket(AF_SP, NN_PAIR);
+	sb = nn_socket(AF_SP, NN_PAIR);
 	if (sb < 0) {
 		printf("Error creating sb socket");
 		return -1;
@@ -23,8 +42,8 @@ int main() {
 	printf("Socket b %d\n", sb);
 
 	// Connect them to the inproc addr
-	int ra = nn_connect(sa, SOCKET_ADDRESS);
-	int rb = nn_bind(sb, SOCKET_ADDRESS);
+	ra = nn_connect(sa, SOCKET_ADDRESS); // Variable number of client
+	rb = nn_bind(sb, SOCKET_ADDRESS); // Should be the data structure server
 	if (ra < 1 || rb < 1) {
 		printf("Error with bind/connect\n");
 		return -1;
@@ -41,14 +60,17 @@ int main() {
 	//printf("Send %d\n", senda);
 	//printf("Receive %d\n", recvb);
 
-	char* data = "data";
-	int len = strlen(data);
+	data = "data";
+	len = strlen(data);
 
 	// Multiple send
-	char* buf = (char*)malloc(len + 1);
-	for (int i = 0; i < 10000; i++) {
-		nn_send(sa, data, len, 0);
+	buf = (char*)malloc(len + 1);
+	for (int i = 0; i < 100000; i++) {
+		nn_send(sa, data, len, NN_DONTWAIT);
 		nn_recv(sb, buf, len + 1, 0);
+		// Insert into linked list
+		Insert(i, list, pos);
+		pos = Advance(pos);
 	}
 	free(buf);
 
