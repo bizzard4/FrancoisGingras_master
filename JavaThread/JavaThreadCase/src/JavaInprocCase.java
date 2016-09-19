b55c6eb6c51701d4391ff005446c1d4a941c9dee
François Gingras <francois.gingras89@gmail.com>
2016-09-19 16:14:58 -0400

    Added time measurement in java case

--- JavaThread/JavaThreadCase/src/JavaInprocCase.java
+++ JavaThread/JavaThreadCase/src/JavaInprocCase.java
@@ -14,43 +14,46 @@ public class JavaInprocCase {
 	 */
 	public static void main(String[] args) {
 		try {
-		// Prepare shared list
-		JavaInprocCase inproc = new JavaInprocCase();
-		
-		int thread_count = Integer.parseInt(args[0]);
-		int update_count = Integer.parseInt(args[1]);
-		int update_size = Integer.parseInt(args[2]); // TODO
-		
-		// Start threads and execute update depending on parameters
-		Runnable torun =new Runnable() {	
-			@Override
-			public void run() {
-				// Number of update per thread
-				for (int i = 0; i < update_count; i++) {
-					synchronized(inproc.list) {
-						inproc.list.add(i); // TODO : Size of update
+			// Prepare shared list
+			JavaInprocCase inproc = new JavaInprocCase();
+			
+			int thread_count = Integer.parseInt(args[0]);
+			int update_count = Integer.parseInt(args[1]);
+			int update_size = Integer.parseInt(args[2]); // TODO
+			
+			// Start threads and execute update depending on parameters
+			Runnable torun =new Runnable() {	
+				@Override
+				public void run() {
+					// Number of update per thread
+					for (int i = 0; i < update_count; i++) {
+						synchronized(inproc.list) {
+							inproc.list.add(i); // TODO : Size of update
+						}
 					}
 				}
+			};
+			
+			// Number of threads
+			long startTime = System.nanoTime();
+			Thread[] threads = new Thread[thread_count];
+			for (int i = 0; i < thread_count; i++) {
+				threads[i] = new Thread(torun);
 			}
-		};
-		
-		// Number of threads
-		Thread[] threads = new Thread[thread_count];
-		for (int i = 0; i < thread_count; i++) {
-			threads[i] = new Thread(torun);
-		}
-		
-		for (int i = 0; i < thread_count; i++) {
-			threads[i].start();
-		}
-
-		for (int i = 0; i < thread_count; i++) {
-			threads[i].join();
-		}
-		
-		// Final stats
-		System.out.println("List size : " + inproc.list.size());
-		
+			
+			//long startTime = System.nanoTime();
+			for (int i = 0; i < thread_count; i++) {
+				threads[i].start();
+			}
+	
+			for (int i = 0; i < thread_count; i++) {
+				threads[i].join();
+			}
+			long endTime = System.nanoTime();
+			
+			// Final timing output
+			System.out.println((endTime - startTime)/1000000.0);
+			
 		} catch (Exception ex) {
 			System.err.println("Error while processing : " + ex.getMessage());
 		}
