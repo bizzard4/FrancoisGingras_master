a7a1888a5eb0e4671b6b01b049cd632bb7c9f455
Ubuntu <ubuntu@ip-172-31-26-55.us-west-2.compute.internal>
2016-09-28 23:21:28 +0000

    Benchmarking script, added csv output and fine tuned script

--- nano_benchmarking/benchmark.py
+++ nano_benchmarking/benchmark.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import sys
 import time
-import subprocess
+import csv
+from subprocess import run, DEVNULL
 
 # Constants
 THREAD_START = 2
@@ -22,14 +23,14 @@ print("Benchmarking for " + torun)
 # Timing function for the java program
 def javaTime(process_name, thread_count, update_count, update_size):
 	start = time.time()
-	subprocess.run(["java", "-cp", "build/", process_name, str(thread_count), str(update_count), str(update_size)])
+	run(["java", "-cp", "build/", process_name, str(thread_count), str(update_count), str(update_size)], stdout=DEVNULL)
 	end = time.time()
 	return int(round((end-start)*1000))
 	
 # Timing function for a native program
 def processTime(command, thread_count, update_count, update_size):
 	start = time.time()
-	subprocess.run([command, str(thread_count), str(update_count), str(update_size)])
+	run(["build/" + command, str(thread_count), str(update_count), str(update_size)], stdout=DEVNULL)
 	end = time.time()
 	return int(round((end-start)*1000))
 
@@ -41,24 +42,33 @@ def timethis(command, thread_count, update_count, update_size, is_java):
 		return processTime(command, thread_count, update_count, update_size)
 	
 # Method to write the result to the output
-def writeRes(test_id, test_params, test_time):
-	print(str(test_id) + ":" + " " + test_params + " time=" + str(test_time) + "ms")
+def writeRes(test_id, test_params, test_time, csvw):
+	csvw.writerow([str(test_id), test_params, str(test_time)])
+	if (test_id % 5 == 0):
+		print("Test id:" + str(test_id))
 	return
 	
+# Execute a test case
 def executeTestCase(command, is_java):
 	# First thread count
-	i = 0
-	for tc in range(THREAD_START, THREAD_MAX, THREAD_STEP):
-		i+=1
-		writeRes(i, "TC=" + str(tc), timethis(command, tc, 1000, 10, is_java))
-	writeRes(i, "TC=" + str(THREAD_MAX), timethis(command, THREAD_MAX, 1000, 10, is_java))
+	with open("output/" + command + "_th.csv", "w", newline="") as thcsv:
+		csvwriter = csv.writer(thcsv, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+		csvwriter.writerow(['test_id', 'test_param', 'test_time'])
+		i = 0
+		for tc in range(THREAD_START, THREAD_MAX, THREAD_STEP):
+			i+=1
+			writeRes(i, "TC=" + str(tc), timethis(command, tc, 1000, 10, is_java), csvwriter)
+		writeRes(i+1, "TC=" + str(THREAD_MAX), timethis(command, THREAD_MAX, 1000, 10, is_java), csvwriter)
 
 	# Update count
-	i = 0
-	for uc in range(UPDATE_COUNT_START, UPDATE_COUNT_MAX, UPDATE_COUNT_STEP):
-		i+=1
-		writeRes(i, "UC=" + str(uc), timethis(command, 4, uc, 10, is_java))
-	writeRes(i, "UC=" + str(UPDATE_COUNT_MAX), timethis(command, 4, UPDATE_COUNT_MAX, 10, is_java))
+	with open("output/" + command + "_uc.csv", "w", newline="") as uccsv:
+		csvwriter = csv.writer(uccsv, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+		csvwriter.writerow(['test_id', 'test_param', 'test_time'])
+		i = 0
+		for uc in range(UPDATE_COUNT_START, UPDATE_COUNT_MAX, UPDATE_COUNT_STEP):
+			i+=1
+			writeRes(i, "UC=" + str(uc), timethis(command, 4, uc, 10, is_java), csvwriter)
+		writeRes(i+1, "UC=" + str(UPDATE_COUNT_MAX), timethis(command, 4, UPDATE_COUNT_MAX, 10, is_java), csvwriter)
 
 	# Update size TODO
 	
@@ -79,7 +89,7 @@ def inproc():
 
 def nano_inproc():
 	print("== nano_inproc starting ==")
-	executeTestCase("build/nano_inproc", False)
+	executeTestCase("nano_inproc", False)
 	print("== nano_inproc done ==")
 	return
 
@@ -91,7 +101,7 @@ def java_inproc():
 
 def pthread_inproc():
 	print("== pthread_inproc starting ==")
-	executeTestCase("build/pthread_inproc", False)
+	executeTestCase("pthread_inproc", False)
 	print("== pthread_inproc done ==")
 	return
 
@@ -137,7 +147,7 @@ target ={
 	"socket_tcp": socket_tcp
 }
 
-# Execute benchmarking
+# Execute the target
 if (torun in target.keys()):
 	target[torun]()
 	print("All done result in output/")
