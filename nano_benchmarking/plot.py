5bf72d687b0aa664e420a01747f11daa95507757
Francois Gingras <francois.gingras89@gmail.com>
2016-11-07 15:48:24 -0500

    Added 07112016 graph, plot script can now plot all

--- nano_benchmarking/plot.py
+++ nano_benchmarking/plot.py
@@ -1,16 +1,18 @@
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 import pandas
 import sys
 
+
 # Usage
 # plot.py ddmmyyyy inproc|ipc|tcp uc|th|us
-
-# TODO
-# - Function to save all (or partial)
+# plot.py ddmmyyyy all
 
 # Print script usage
 def printUsage():
 	print("Usage : plot.py ddmmyyyy inproc|ipc|tcp uc|th|us")
+	print("Usage : plot.py ddmmyyyy all")
 	return
 
 # Add a TH line on a plot
@@ -25,30 +27,48 @@ xlabel_list = {"th" : "Thread count", "uc" : "Update count", "us" : "Update size
 sign_list = [".r", ".b", ".g"] # At most, 3 sign on the plot in the same time
 
 # Filter parameters
-if (len(sys.argv) < 4):
+plot_order = []
+if (len(sys.argv)==4): # 2 params
+	tech = sys.argv[2]
+	if((tech != "inproc") and (tech != "ipc") and (tech != "tcp")):
+		print("Wrong tech : inproc, ipc, tcp")
+		printUsage()
+		sys.exit(-1)
+	param_type = sys.argv[3]
+	if((param_type != "uc") and (param_type != "th") and (param_type != "us")):
+		print("Wrong param type : th, uc, us")
+		printUsage()
+		sys.exit(-1)
+	plot_order.append((tech, param_type))
+elif (len(sys.argv)==3): # All
+	if (sys.argv[2] != "all"):
+		print("Wrong params : all?")
+		printUsage()
+		sys.exit(-1)
+
+	# Add all plot order
+	for tech_val in tech_list:
+		for label_val in xlabel_list:
+			if (label_val != "us"): # us not implemented for now
+				plot_order.append((tech_val, label_val))
+else:
 	printUsage()
 	sys.exit(-1)
+
 result_set = sys.argv[1]
-tech = sys.argv[2]
-if((tech != "inproc") and (tech != "ipc") and (tech != "tcp")):
-	print("Wrong tech : inproc, ipc, tcp")
-	printUsage()
-	sys.exit(-1)
-param_type = sys.argv[3]
-if((param_type != "uc") and (param_type != "th") and (param_type != "us")):
-	print("Wrong param type : th, uc, us")
-	printUsage()
-	sys.exit(-1)
 
 # Plot the requested chart
-to_plot = tech_list[tech]
-count = 0
-for val in to_plot:
-	plotResult("results/" + result_set + "/" + val + "_" + param_type + ".csv", sign_list[count], tech.upper() + " " + param_type.upper())
-	count += 1
+print(plot_order)
+for order in plot_order:
+	to_plot = tech_list[order[0]]
+	count = 0
+	for val in to_plot:
+		plotResult("results/" + result_set + "/" + val + "_" + order[1] + ".csv", sign_list[count], val.upper() + " " + order[1].upper())
+		count += 1
 
-# Plot all
-plt.ylabel("Time (ms)")
-plt.xlabel(xlabel_list[param_type])
-plt.legend(bbox_to_anchor=(0.05, 0.95), loc=2, borderaxespad=0.)
-plt.savefig("test.png")
+	# Plot all
+	plt.ylabel("Time (ms)")
+	plt.xlabel(xlabel_list[order[1]])
+	plt.legend(bbox_to_anchor=(0.05, 0.95), loc=2, borderaxespad=0.)
+	plt.savefig("results/" + result_set + "/" + order[0] + "_" + order[1] + ".png")
+	plt.gcf().clear()
