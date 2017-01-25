53317037f49489e3b9c1819ace23f83d38613f18
Francois Gingras <francois.gingras89@gmail.com>
2017-01-25 18:48:14 -0500

    Samplesort python, added basic syncrhonization

--- exploration/samplesort/ss.py
+++ exploration/samplesort/ss.py
@@ -1,5 +1,7 @@
 import threading
 import copy
+import asyncio
+import math
 
 # Sample sort implementation in Python for the purpose of understanding the concept
 
@@ -12,23 +14,120 @@ import copy
 
 # Algorithm (from Todd email)
 
+# Global variable (need lock and reset)
+root_lock = None
+root_sample = None
+root_sample_received = None
+root_sample_event = None
+threads = None
+
+# Represent a task (bucket) for the sample sort
 class ss_task(threading.Thread):
-	def __init__(self, threadID, name, data, N, K):
+	def __init__(self, threadID, name, data, N, SN, K, global_lock, barrier_lock):
 		threading.Thread.__init__(self)
 		self.threadID = threadID
 		self.name = name
 		self.data = data
 		self.N = N
+		self.SN = SN
 		self.K = K
+		self.global_lock = global_lock
+		self.barrier_lock = barrier_lock
+		self.splitters = None
 
 	def run(self):
-		#print("Starting " + self.name)
-		print(self.data)
+		global root_sample, root_sample_received, root_sample_event, threads
+
+		# Sort data (nlogn)
 		self.data = sorted(self.data)
-		print(self.data)
-		#print("Exiting " + self.name)
+		print("STARTING", self.name, "-", self.data)
 
+		# Gather sample and send them to the main method (using lock)
+		step = int(self.SN / self.K)
+		sample_to_send = []
+		print("SAMPLING", self.name, "-",  step)
+		for ni in range(0, self.SN, step):
+			sample_to_send.append(self.data[ni])		
 
+		self.global_lock.acquire()
+		print(root_sample_received, root_sample)
+		root_sample.extend(sample_to_send)
+		root_sample_received += 1
+		if (root_sample_received == self.K):
+			root_sample_event.set()
+		self.global_lock.release()
+
+		# Wait on splitters information
+		print("Barrier waiting with", self.barrier_lock.n_waiting, "others")
+		self.barrier_lock.wait()
+
+		# Go throught all data and send them at the right task
+		print("SWAPPING", self.name, "-", self.splitters)
+		for val in self.data:
+			for i in range(len(self.splitters)):
+				if val < self.splitters[i]:
+					threads[i].insertValue(val)
+					break
+
+	# In final version, this would do a insertion sort, but here we will just do append and sorted
+	def insertValue(self, newVal):
+		self.data.append(newVal)
+		self.data = sorted(self.data)
+
+	def setSplitter(self, splitters):
+		self.splitters = splitters
+
+# Return sorted array (copy of)
+def samplesort(data, N, K):
+	global root_sample, root_sample_received, root_sample_event, threads
+
+	print("Full data", data)
+
+	# Reset global data
+	root_sample = []
+	root_lock = threading.Lock()
+	root_sample_received = 0
+	root_sample_event = threading.Event()
+	threads = [None]*K
+
+	try:
+		#size_per_task = round(N/K) # Rounding (do not floor)
+		size_per_task = int(N/K)
+		barrier = threading.Barrier(K+1)
+
+		current_index = 0
+		for ki in range(K):
+			data_to_task = copy.deepcopy(data[current_index:current_index+size_per_task])
+
+			# Add rest of the array to the last one
+			if ki == K-1:
+				data_to_task.extend(copy.deepcopy(data[current_index+size_per_task:]))
+
+			threads[ki] = ss_task(ki, "Task " + str(ki), data_to_task, N, size_per_task, K, root_lock, barrier)
+			threads[ki].start()
+			current_index += size_per_task
+
+		# Wait for all sample from all task (event synchronization)
+		root_sample_event.wait()
+		print("Got all sample", root_sample)
+
+		# Compute and send splitters info to all task
+		root_sample = sorted(root_sample)
+		for ki in range(K):
+			threads[ki].setSplitter([1, 7])
+
+		# Unlock barrier, so all thread can start swapping (barrier synchronization)
+		barrier.wait()
+
+		# Wait for all thread to be done
+		for ki in range(K):
+			threads[ki].join()
+
+		print("Done")
+	except Exception as e:
+		print("Error while creating threads", e)
+
+# Test input
 
 input_1 = [2, 5, 8, 6, 3, 1, 7, 9, 10, 4]
 input_1_n = len(input_1) #10
@@ -38,30 +137,16 @@ input_2 = [2, 5, 8, 6, 3, 1, 7, 9, 10, 4]
 input_2_n = len(input_1) #10
 k_2 = 3
 
-# Return sorted array (copy of)
-def samplesort(data, N, K):
-	print("Full data", data)
-	try:
-		size_per_task = int(N/K) # Floor
-		threads = [None]*K
-		current_index = 0
-		for ki in range(K):
-			data_to_task = copy.deepcopy(data[current_index:current_index+size_per_task])
+input_3 = []
+for i in range(19):
+	input_3.append(i)
+input_3_n = len(input_3)
+k_3 = 4
 
-			if ki == K-1:
-				data_to_task.extend(copy.deepcopy(data[current_index+size_per_task:])) # Rest of the array
-
-			threads[ki] = ss_task(ki, "Task " + str(ki), data_to_task)
-			threads[ki].start()
-			current_index += size_per_task
-
-		for ki in range(K):
-			threads[ki].join()
-
-		print("Done")
-	except Exception as e:
-		print("Error while creating threads", e)
+# Test run
 
 samplesort(input_1, input_1_n, k_1)
 print()
-samplesort(input_2, input_2_n, k_2)
+#samplesort(input_2, input_2_n, k_2)
+print()
+#samplesort(input_3, input_3_n, k_3)
