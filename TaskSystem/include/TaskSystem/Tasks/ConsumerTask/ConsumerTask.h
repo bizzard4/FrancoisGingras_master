6133a39bb02e0bb135d261cb5f0815777aa0b721
Francois Gingras <francois.gingras89@gmail.com>
2017-05-02 14:27:13 -0400

    Deleted old producer consumer, and unbounded Q

--- TaskSystem/include/TaskSystem/Tasks/ConsumerTask/ConsumerTask.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef ConsumerTask_H_
-#define ConsumerTask_H_
-
-typedef struct ConsumerTask *ConsumerTask;
-unsigned int ConsumerTask_create();
-
-#endif /* ConsumerTask_H_ */
--- TaskSystem/include/TaskSystem/Tasks/ProducerTask/ProducerTask.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef ProducerTask_H_
-#define ProducerTask_H_
-
-typedef struct ProducerTask *ProducerTask;
-unsigned int ProducerTask_create();
-
-#endif /* ProducerTask_H_ */
