c525a08e42071d6dc4370b01be941d49ac81730a
Francois Gingras <francois.gingras89@gmail.com>
2017-06-02 15:51:02 -0400

    Updated script to run multiple client on database

--- TaskSystem/tool/db_run.sh
+++ TaskSystem/tool/db_run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "Running multiple databas client N=$1"
+echo "Running multiple databas client N=$1 Req=$2"
 
 
 
 echo "Running code ->"
 for n in $(seq 1 $1)
 do
-	test/db_client_test > db_out_$n.txt &
+	test/db_client_test $2 > db_out_$n.txt &
 done
 
 wait
